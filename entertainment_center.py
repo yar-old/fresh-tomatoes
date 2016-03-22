fc00e79bf18ae2553ac9dbd558f672bc07c6cfdb
yramocan <yramocan@umich.edu>
2016-03-21 23:04:55 -0400

    Add extra movie info. Modify HTML layout.

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,19 +8,28 @@ from fresh_tomatoes import open_movies_page
 spirited_away = media.Movie(
     "Spirited Away", # Title
     "http://www.nausicaa.net/miyazaki/sen/poster_images/USA_full.jpg", # Image
-    "https://www.youtube.com/watch?v=ByXuk9QqQkk" # Trailer
+    "https://www.youtube.com/watch?v=ByXuk9QqQkk", # Trailer
+    "20 September 2002",
+    "Hayao Miyazaki",
+    "Hayao Miyazaki"
 )
 
 mad_max = media.Movie(
     "Mad Max: Fury Road",
     "https://www.movieposter.com/posters/archive/main/201/MPW-100532",
-    "https://www.youtube.com/watch?v=hEJnMQG9ev8"
+    "https://www.youtube.com/watch?v=hEJnMQG9ev8",
+    "15 May 2015",
+    "George Miller",
+    "George Miller, Brendan McCarthy, Nick Lathouris"
 )
 
 zootopia = media.Movie(
     "Zootopia",
     "http://cdn3-www.comingsoon.net/assets/uploads/gallery/zootopia/c55463169023e916571b0361c592cd6c0f630904.jpg",
-    "https://www.youtube.com/watch?v=bY73vFGhSVk"
+    "https://www.youtube.com/watch?v=bY73vFGhSVk",
+    "4 March 2016",
+    "Byron Howard, Rich Moore, Jared Bush",
+    "Jared Bush, Phil Johnston"
 )
 
 #############################################
--- media.py
+++ media.py
@@ -1,6 +1,10 @@
 class Movie(object):
     # Constructor for Movie Class
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_youtube_url,
+                 release_date, director, screenwriter):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.release_date = release_date
+        self.director = director
+        self.screenwriter = screenwriter
